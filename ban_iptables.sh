455a3c8946800aecb07619ecce182a7d3c7bdad2
Subeting <68261543+Subeting@users.noreply.github.com>
2021-07-15 12:45:00 +0800

    Update ban_iptables.sh

--- ban_iptables.sh
+++ ban_iptables.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-yum install iptables-services -y
-
-
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 #=================================================
@@ -35,11 +32,7 @@ magnet:
 xunlei
 sandai
 Thunder
-XLLiveUD
-tor-exit
-torservers
-torland.is
-torproject"
+XLLiveUD"
 
 check_sys(){
 	if [[ -f /etc/redhat-release ]]; then
@@ -68,7 +61,7 @@ check_SPAM(){
 	SPAM_PORT=$(echo -e "$Ban_PORT_list"|grep "${smtp_port}")
 }
 Cat_PORT(){
-	Ban_PORT_list=$(iptables -t filter -L OUTPUT -nvx --line-numbers|grep "DROP"|grep "udp"|awk '{print $13}')
+	Ban_PORT_list=$(iptables -t filter -L OUTPUT -nvx --line-numbers|grep "REJECT"|awk '{print $13}')
 }
 Cat_KEY_WORDS(){
 	Ban_KEY_WORDS_list=""
@@ -115,10 +108,10 @@ Save_iptables_v4_v6(){
 		chmod +x /etc/network/if-pre-up.d/iptables
 	fi
 }
-Set_key_word() { $1 -t mangle -$3 OUTPUT -m string --string "$2" --algo kmp --to 65535 -m state --state NEW -j DROP; }
+Set_key_word() { $1 -t mangle -$3 OUTPUT -m string --string "$2" --algo bm --to 65535 -j DROP; }
 Set_tcp_port() {
-	[[ "$1" = "$v4iptables" ]] && $1 -t filter -$3 OUTPUT -p tcp -m multiport --dports "$2" -m state --state NEW,ESTABLISHED -j DROP
-	[[ "$1" = "$v6iptables" ]] && $1 -t filter -$3 OUTPUT -p tcp -m multiport --dports "$2" -m state --state NEW,ESTABLISHED -j DROP
+	[[ "$1" = "$v4iptables" ]] && $1 -t filter -$3 OUTPUT -p tcp -m multiport --dports "$2" -m state --state NEW,ESTABLISHED -j REJECT --reject-with icmp-port-unreachable
+	[[ "$1" = "$v6iptables" ]] && $1 -t filter -$3 OUTPUT -p tcp -m multiport --dports "$2" -m state --state NEW,ESTABLISHED -j REJECT --reject-with tcp-reset
 }
 Set_udp_port() { $1 -t filter -$3 OUTPUT -p udp -m multiport --dports "$2" -j DROP; }
 Set_SPAM_Code_v4(){
